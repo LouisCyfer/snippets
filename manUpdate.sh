7974da028c717aba53a1d78d0fa814acd9ba308b
LouisCyfer <cyfer3@googlemail.com>
2016-06-29 12:57:19 +0200

    Update manUpdate.sh

--- manUpdate.sh
+++ manUpdate.sh
@@ -6,26 +6,27 @@ version="v0.2"
 
 askUser ()
 {
-	runCmd=true
+    runCmd=true
 
-	if [ $askAlways = true ]; then
-		response=""
-		echo ""
-		read -r -p "Ausführen von 'apt-get $1'? (j/n ENTER-Taste=n)" response
-		#response=${response,,}
+    if [ $askAlways = true ]; then
+	response=""
+	echo ""
+	read -r -p "Ausführen von 'apt-get $1'? (j/n ENTER-Taste=n)" response
 
-		case $response in
-		    j|J) runCmd=true;;
-		    *) runCmd=false;;
-		esac
+	case $response in
+	j|J)
+	    runCmd=true;;
+	*)
+	    runCmd=false;;
+	esac
 
-	fi
-	
-	if [ $runCmd = true ]; then
-		echo "" && echo "--> starte apt-get $1" && sleep 1s && sudo apt-get $1
-	else
-		echo "" && echo "--> überspringe apt-get $1" && sleep 1s
-	fi
+    fi
+
+    if [ $runCmd = true ]; then
+	echo "" && echo "--> starte apt-get $1" && sleep 1s && sudo apt-get $1
+    else
+	echo "" && echo "--> überspringe apt-get $1" && sleep 1s
+    fi
 }
 
 RESULT=$(dialog --title "manUpdate.sh $version" --menu "Dieses script wird folgende ’apt-get’-Kommandos ausführen\nupdate, upgrade, dist-upgrade, autoremove, autoclean\n(in dieser Reihenfolge)\n\nBitte Wählen Sie eine Option:" 15 70 16 1 "genau so fortfahren" 2 "fortfahren, aber vor jedem Kommando fragen" 3 "abbrechen" 3>&1 1>&2 2>&3 3>&-);
@@ -33,21 +34,23 @@ RESULT=$(dialog --title "manUpdate.sh $version" --menu "Dieses script wird folge
 clear
 
 case $RESULT in
-	1) echo "" && echo "überspringe keine der Kommandos." && echo ""  && sleep 2s;;
-	2) askAlways=true;;
-	*) echo "" && echo "breche Vorgang ab." && cancelHit=true;;
+1)
+    echo "" && echo "überspringe keine der Kommandos." && echo ""  && sleep 2s ;;
+2)
+    askAlways=true ;;
+*)
+    echo "" && echo "breche Vorgang ab." && cancelHit=true ;;
 esac
 
-if [ $cancelHit = false ];
-then
-	echo "" && echo "*Update/Upgrade*" && sleep 1s
-	askUser "update"
-	askUser "upgrade"
-	askUser "dist-upgrade"
+if [ $cancelHit = false ]; then
+    echo "" && echo "*Update/Upgrade*" && sleep 1s
+    askUser "update"
+    askUser "upgrade"
+    askUser "dist-upgrade"
 
-	echo "" && echo "*Aufräumen*" && sleep 1s
-	askUser "autoremove"
-	askUser "autoclean"
+    echo "" && echo "*Aufräumen*" && sleep 1s
+    askUser "autoremove"
+    askUser "autoclean"
 fi
 
 echo "" && echo "manUpdate.sh $version --> fertig. :)"
