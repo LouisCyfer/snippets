4f8449cb86ef10fe891a2df127e1958366023bbe
LouisCyfer <cyfer3@googlemail.com>
2016-07-01 03:53:22 +0200

    no need to clean RAM afterwards (check out my RAM cleaner :P)

--- runKSP.sh
+++ runKSP.sh
@@ -99,37 +99,6 @@ else
 	echo "[:: starting ckan.exe (Mono) ::]"
 	cd "$KSPpath" && mono "ckan.exe" && cd ;;
     esac
-
-    sleep 1s
-
-    echo ""
-    echo "--> Do you wish to free your RAM now by one of the following commands?"
-
-    optionID=0
-    option1="run 'sudo sysctl -w vm.drop_caches=3'"
-    option2="run 'sudo sync && echo 3 | sudo tee /proc/sys/vm/drop_caches'"
-    option3="don't free RAM"
-
-    response=$( zenity --title="Question" --width=550 --height=250 --list --radiolist --text="" \
-    --column="" --column="pick one of the following options" FALSE "$option1" FALSE "$option2" TRUE "$option3" )
-
-    case "$response" in
-    $option1 )
-	optionID=1
-	echo "running 'sudo sysctl -w vm.drop_caches=3' ..."
-	sudo sysctl -w vm.drop_caches=3 ;;
-    $option2 )
-	optionID=2
-	echo "running 'sudo sync && echo 3 | sudo tee /proc/sys/vm/drop_caches' ..."
-	sudo sync && echo 3 | sudo tee /proc/sys/vm/drop_caches ;;
-    $option3 )
-	optionID=3
-	echo "no action taken" ;;
-    esac
-
-    if [ $optionID = 0 ]; then
-	echo "no action taken"
-    fi
     sleep 5s
 fi
 
