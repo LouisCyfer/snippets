9fb71cc9cc14d59a625ca7f8887f63bced298bb3
LouisCyfer <LouisCyfer@users.noreply.github.com>
2016-12-15 06:12:41 +0100

    some quick changes

--- runKSP.sh
+++ runKSP.sh
@@ -1,35 +1,78 @@
 #!/bin/bash
 
-#NOTE: please change the folders to your advantage!
-Kpaths[0]="/mnt/.../Kerbal Space Program_1.1.2" # folder 1 (for me its the backup of KSP v1.1.2)
-Kpaths[1]="/mnt/.../Kerbal Space Program_1.1.3" # folder 2 (same as above but KSP v1.1.3)
-Kpaths[2]="/mnt/.../Kerbal Space Program_current" # folder 3
-Kpaths[3]="/mnt/.../Kerbal Space Program_test" # folder 3
-
 KSPpath=""
+prePath="/mnt/...." #change this to your preference (i have several sub-folders, so the pre-folder is the same)
 
-KSPapp="KSP.x86_64" # erase the '_64' if your want to run the 32bit version of KSP
-kspParams="-force-opengl" #"-force-opengl" "-force-opengl -force-gfx-direct"
+#if you add/remove, you have to edit the zenity-response variable below
+Kpaths[0]="Kerbal Space Program_current"
+Kpaths[1]="Kerbal Space Program_test"
+Kpaths[2]="Kerbal Space Program_1.1.2"
+Kpaths[3]="Kerbal Space Program_1.1.3"
+Kpaths[4]="Kerbal Space Program_1.2.1"
+
+maxPathes=${#Kpaths[@]}
+
+KSPapp="KSP.x86"
+found64bit=false
+
+if [ "$(uname -m)" = "x86_64" ]; then
+    KSPapp=$KSPapp"_64"
+    found64bit=true
+fi
+
+#possible params --> -force-opengl -force-opengl -force-gfx-direct
+kspParams="-force-opengl"
+# kspParams=""
+
+#
+preRun="taskset -c 2-3"
 
 goAhead=true
 optionID=0
 kspPID=0
 
 clear
-echo "[:: KSP Launcher ::]"
+
+msg="32bit"
+if [ "$found64bit" = true ]; then
+    msg="64bit"
+fi
+
+echo "found $msg OS $(uname -s) --> going to run $KSPapp $kspParams"
+echo ""
+echo "found $maxPathes path entries:"
+
+optionID=0
+options[0]=""
+optionsStr=""
+
+idx=0
+# ${#Kpaths[@]}
+
+while [ $idx -lt $maxPathes ]; do
+    options[$idx]="$prePath${Kpaths[$idx]}"
+
+    if [ $idx = $(($maxPathes -1)) ]; then
+	optionsStr="$optionsStr${options[$idx]}"
+    else
+	optionsStr="$optionsStr${options[$idx]}|"
+    fi
+
+    echo "--> ${options[$idx]}/"
+
+    let idx=idx+1
+    # idx=$(($idx + 1))
+done
+
+echo ""
+echo "[:: KSP Launcher | pick path ::]"
 echo ""
 echo "--> Wich folder you wish to use?"
 
-optionID=0
-
-options[0]="run from ${Kpaths[0]}"
-options[1]="run from ${Kpaths[1]}"
-options[2]="run from ${Kpaths[2]}"
-options[3]="run from ${Kpaths[3]}"
-
-response=$( zenity --title="Question" --width=600 --height=250 --list --radiolist --text="" \
---column="" --column="pick one of the following options" \
-FALSE "${options[0]}" FALSE "${options[1]}" TRUE "${options[2]}" FALSE "${options[3]}" )
+#if you addeed/removed, you have to edit this here: (i might have to figure a better way later)
+response=$(zenity --title="[:: KSP Launcher | pick path ::]" --width=600 --height=250 --list --radiolist --text="" \
+--column="" --column="please pick a path" \
+TRUE "${options[0]}" FALSE "${options[1]}" FALSE "${options[2]}" FALSE "${options[3]}" FALSE "${options[4]}")
 
 case "$response" in
 ${options[0]} )	
@@ -40,22 +83,17 @@ ${options[2]} )
     optionID=3 ;;
 ${options[3]} )		
     optionID=4 ;;	    
+${options[4]} )		
+    optionID=5 ;;	    
 esac
 
 if [ $optionID = 0 ]; then
     goAhead=false
 
 else
-    case "$response" in
-    ${options[0]} )
-	KSPpath=${Kpaths[0]} ;;
-    ${options[1]} )
-	KSPpath=${Kpaths[1]} ;;
-    ${options[2]} )
-	KSPpath=${Kpaths[2]} ;;
-    ${options[3]} )
-	KSPpath=${Kpaths[3]} ;;	    
-    esac
+    pickID=$(($optionID - 1))
+    KSPpath=$response
+    # KSPpath=$prePath${Kpaths[$(($optionID - 1))]}
 
     echo "using path '$KSPpath'"
     echo ""
@@ -65,7 +103,7 @@ else
     options[0]="run KSP"
     options[1]="run ckan.exe per mono"
 
-    response=$( zenity --title="Question" --width=600 --height=250 --list --radiolist --text="" \
+    response=$( zenity --title="[:: KSP Launcher | run application ::]" --width=600 --height=250 --list --radiolist --text="" \
     --column="" --column="pick one of the following options" TRUE "${options[0]}" FALSE "${options[1]}" )
 
     case "$response" in
@@ -87,16 +125,20 @@ else
 	echo ""
 	
 	cd "$KSPpath"
-	# cd "$KSPpath" && ./$KSPapp &
 	
-	echo 'exporting LD_PRELOAD="libpthread.so.0 libGL.so.1"'
-	export LD_PRELOAD="libpthread.so.0 libGL.so.1"
+	echo 'running --> export LC_ALL=C'
+	export LC_ALL=C
 
-	echo 'exporting __GL_THREADED_OPTIMIZATIONS=1'
+	# echo 'running --> export LD_PRELOAD="libpthread.so.0 libGL.so.1"'
+	# export LD_PRELOAD="libpthread.so.0 libGL.so.1"
+
+	echo 'running --> export __GL_THREADED_OPTIMIZATIONS=1'
 	export __GL_THREADED_OPTIMIZATIONS=1
-	
-	echo "running with params $kspParams"
-	./$KSPapp $kspParams &
+
+	echo "using $preRun and running with params $kspParams"
+	echo "executing > $preRun ./$KSPapp $kspParams &"
+	sleep 2s
+	$preRun ./$KSPapp $kspParams &
 
 	echo ""
 	sleep 1s
